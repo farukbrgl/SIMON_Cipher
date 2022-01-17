2cb6d2bbf08867c93f82fb214d95815ad5926f2a
Ömer Faruk Birgül <birgulo@itu.edu.tr>
2022-01-17 12:49:31 +0300

    update

--- python/simon_prng.py
+++ python/simon_prng.py
@@ -13,10 +13,13 @@ bunlar orijinal makaledeki n=16, m=4 durumunda verilen başlangıç vektörleri
 sonuçların doğruluğunu makaleden inceleyebilirsiniz
 farklı boyuttaki şifreleyici için farklı değerler vermelisiniz
 """
-def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, key_3 = 0x1f1e1d1c1b1a1918, key_2 = 0x1716151413121110, key_1 = 0x0f0e0d0c0b0a0908, key_0 = 0x0706050403020100, n = 64, m = 4):
+
+
+def SIMON(plainText_1=0x74206e69206d6f6f, plainText_2=0x6d69732061207369, key_3=0x1f1e1d1c1b1a1918, key_2=0x1716151413121110, key_1=0x0f0e0d0c0b0a0908, key_0=0x0706050403020100, n=64, m=4):
     # plainText_1 = 0x6373656420737265 #most significant
     # plainText_2 = 0x6c6c657661727420 #least significant
-    print (format(plainText_1, '0X'), format(plainText_2, "0X"), "plaintexts in start in hex")
+    print(format(plainText_1, '0X'), format(
+        plainText_2, "0X"), "plaintexts in start in hex")
 
     # cipherText_2 = 0x49681b1e1e54fe3f
     # cipherText_1 = 0x65aa832af84e0bbc
@@ -26,18 +29,23 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
     # key_2 = 0x121110
     # key_1 = 0x0f0e0d0c0b0a0908
     # key_0 = 0x0706050403020100 #least significant
-    print (format(key_3, '0x'), format(key_2, "0X"), format(key_1, "0X"), format(key_0, "0X"),"keys in hex")
+    print(format(key_3, '0x'), format(key_2, "0X"), format(
+        key_1, "0X"), format(key_0, "0X"), "keys in hex")
 
-    #z sabitleri oluşturuluyor
-    z_0 = [1,1,1,1,1,0,1,0,0,0,1,0,0,1,0,1,0,1,1,0,0,0,0,1,1,1,0,0,1,1,0,1,1,1,1,1,0,1,0,0,0,1,0,0,1,0,1,0,1,1,0,0,0,0,1,1,1,0,0,1,1,0]
-    z_1 = [1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,0,0,0,0,1,0,1,1,0,1,0,1,0,0,0,1,1,1,0,1,1,1,1,1,0,0,1,0,0,1,1,0,0,0,0,1,0,1,1,0,1,0]
-    z_2 = [1,0,1,0,1,1,1,1,0,1,1,1,0,0,0,0,0,0,1,1,0,1,0,0,1,0,0,1,1,0,0,0,1,0,1,0,0,0,0,1,0,0,0,1,1,1,1,1,1,0,0,1,0,1,1,0,1,1,0,0,1,1]
-    z_3 = [1,1,0,1,1,0,1,1,1,0,1,0,1,1,0,0,0,1,1,0,0,1,0,1,1,1,1,0,0,0,0,0,0,1,0,0,1,0,0,0,1,0,1,0,0,1,1,1,0,0,1,1,0,1,0,0,0,0,1,1,1,1]
-    z_4 = [1,1,0,1,0,0,0,1,1,1,1,0,0,1,1,0,1,0,1,1,0,1,1,0,0,0,1,0,0,0,0,0,0,1,0,1,1,1,0,0,0,0,1,1,0,0,1,0,1,0,0,1,0,0,1,1,1,0,1,1,1,1]
+    # z sabitleri oluşturuluyor
+    z_0 = [1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1,
+           0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0]
+    z_1 = [1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1,
+           0, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0]
+    z_2 = [1, 0, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0,
+           0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 1, 1]
+    z_3 = [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0,
+           0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1]
+    z_4 = [1, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0,
+           0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1]
 
-
-    #n ve m değerleri oluşturuluyor
-    #bir alttaki bölümde el ile n ve m değeri girmeyi ayarlanabilir
+    # n ve m değerleri oluşturuluyor
+    # bir alttaki bölümde el ile n ve m değeri girmeyi ayarlanabilir
     # n = 16
     # m = 4
 
@@ -49,9 +57,8 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
     # n = int(input("n değerini giriniz:"))
     # m = int(input("m değerini giriniz:"))
 
-    ##sabit değer
+    # sabit değer
     c = 2**(n) - 1
-
 
     T = 0
     j = 0
@@ -88,8 +95,8 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
     else:
         print("yanlış değer girdiniz\ndevam etmeyiniz")
 
+    key_0_list = []
 
-    key_0_list= []
     def key_generation(key_0, key_1, key_2, key_3, m, T):
         key_0_n = key_0
         key_1_n = key_1
@@ -97,8 +104,8 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
         key_3_n = key_3
         if (m == 2):
             for i in range(T):
-                temp1 = (key_1 >> 3)|(key_1 << (n - 3)) & c
-                temp2 = (temp1 >> 1)|(temp1 << (n - 1)) & c
+                temp1 = (key_1 >> 3) | (key_1 << (n - 3)) & c
+                temp2 = (temp1 >> 1) | (temp1 << (n - 1)) & c
                 temp3 = key_0_n ^ temp1
                 temp4 = temp2 ^ temp3
                 temp5 = temp4 ^ c ^ ((z_2[i % 62])) ^ 3
@@ -110,8 +117,8 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
             return key_0_n, key_1
         elif (m == 3):
             for i in range(T):
-                temp1 = (key_2 >> 3)|(key_2 << (n - 3)) & c
-                temp2 = (temp1 >> 1)|(temp1 << (n - 1)) & c
+                temp1 = (key_2 >> 3) | (key_2 << (n - 3)) & c
+                temp2 = (temp1 >> 1) | (temp1 << (n - 1)) & c
                 temp3 = key_0_n ^ temp1
                 temp4 = temp2 ^ temp3
                 if (n == 24):
@@ -133,9 +140,9 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
             for i in range(T):
                 # print ("key{} = {}".format(i, key_0_n))
                 # print ("key_0_n in hex",format(key_0_n, '016b'))
-                temp1 = (key_3 >> 3)|(key_3 << (n - 3)) & c
+                temp1 = (key_3 >> 3) | (key_3 << (n - 3)) & c
                 temp2 = key_1 ^ temp1
-                temp3 = (temp2 >> 1)|(temp2 << (n - 1)) & c
+                temp3 = (temp2 >> 1) | (temp2 << (n - 1)) & c
                 temp4 = key_0_n ^ temp2
                 temp5 = temp3 ^ temp4
                 if (n == 16):
@@ -155,20 +162,20 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
                 # print ("key{} = {}".format(i, key_next)
             return key_0_list
 
-    key_list =key_generation(key_0, key_1, key_2, key_3, m, T)
+    key_list = key_generation(key_0, key_1, key_2, key_3, m, T)
 
-    ### encryption
-    text1_list= []
-    text2_list= []
+    # encryption
+    text1_list = []
+    text2_list = []
     for k in key_0_list:
-        ### plaintext = t1[0:3]t2[0:3]
+        # plaintext = t1[0:3]t2[0:3]
         t1 = plainText_1
         t2 = plainText_2
         # print ("before texts 01 in hex",format(t1, '04x'), format(t2, "04X"))
         # print(key_0_list[i])
-        crol_1 = (t1 << 1) + (t1 >> (n-1)) & c
-        crol_2 = (t1 << 2) + (t1 >> (n-2)) & c
-        crol_8 = (t1 << 8) + (t1 >> (n-8)) & c
+        crol_1 = (t1 << 1) + (t1 >> (n - 1)) & c
+        crol_2 = (t1 << 2) + (t1 >> (n - 2)) & c
+        crol_8 = (t1 << 8) + (t1 >> (n - 8)) & c
         tmp1 = crol_1 & crol_8
         tmp2 = t2 ^ tmp1
         tmp3 = tmp2 ^ crol_2
@@ -180,10 +187,9 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
         plainText_1 = t1
         plainText_2 = t2
         # return text1_list, text2_list
-    print (format(text1_list[-1], '0X'), format(text2_list[-1], "0X"), "ciphertext hex",)
-    print ("********************************************************************")
-
-
+    print(format(text1_list[-1], '0X'),
+          format(text2_list[-1], "0X"), "ciphertext hex",)
+    print("********************************************************************")
 
     """
     ### decryption
@@ -229,6 +235,7 @@ def SIMON(plainText_1 = 0x74206e69206d6f6f, plainText_2 = 0x6d69732061207369, ke
     # a,b= enc(plainText_1, plainText_2, key_list, T)
     return cipherText_1, cipherText_2
 
+
 cipherText_1 = 0
 cipherText_2 = 0
 IV_1 = 0x74206e69206d6f6f
@@ -238,15 +245,15 @@ IV_2 = 0x6d69732061207369
 # print(pt_1)
 # print(pt_2)
 f = open("random_numbers.txt", "w")
-a,b = SIMON(plainText_1 = IV_1, plainText_2 = IV_2)
+a, b = SIMON(plainText_1=IV_1, plainText_2=IV_2)
 x = format(a, '064b')
 y = format(b, '064b')
-f.write(str(x)+str(y)+"\n")
-for i in range (9):
-    a,b = SIMON(plainText_1 = a, plainText_2 = b)
+f.write(str(x) + str(y) + "\n")
+for i in range(100):
+    a, b = SIMON(plainText_1=a, plainText_2=b)
     z = format(a, '064b')
     t = format(b, '064b')
-    f.write(str(a)+str(t)+"\n")
+    f.write(str(a) + str(t) + "\n")
 f.close()
 # while 1 == 1:
 #     a,b = SIMON(plainText_1 = a, plainText_2 = b)
