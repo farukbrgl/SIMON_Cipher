e7f19f5991c3c0a184ccd517c02abdc3cff9f3bf
Ömer Faruk Birgül <birgulo@itu.edu.tr>
2022-03-15 20:46:47 +0300

    update simon and generated tables

--- python/simon_prng.py
+++ python/simon_prng.py
@@ -9,14 +9,14 @@ Orijinal makaleye https://eprint.iacr.org/2013/404 adresinden ulaşabilirsiniz.
 
 """
 plaintext, ciphertext ve key oluşturuldu
-encryption ve decryption işlemlerinin ikisi de gerçekleştiriliyor
-bunlar orijinal makaledeki n=16, m=4 durumunda verilen başlangıç vektörleri
+encryption işlemi gerçekleştiriliyor
+bunlar orijinal makaledeki n=64, m=4 durumunda verilen başlangıç vektörleri
 sonuçların doğruluğunu makaleden inceleyebilirsiniz
 farklı boyuttaki şifreleyici için farklı değerler vermelisiniz
 """
 
 
-def SIMON(plainText_1=0x74206e69206d6f6f, plainText_2=0x6d69732061207369, key_3=0x1f1e1d1c1b1a1918, key_2=0x1716151413121110, key_1=0x0f0e0d0c0b0a0908, key_0=0x0706050403020100, n=64, m=4):
+def SIMON(plainText_1, plainText_2, key_3, key_2, key_1, key_0, n=64, m=4):
     # plainText_1 = 0x6373656420737265 #most significant
     # plainText_2 = 0x6c6c657661727420 #least significant
     print(format(plainText_1, '0X'), format(
@@ -26,10 +26,10 @@ def SIMON(plainText_1=0x74206e69206d6f6f, plainText_2=0x6d69732061207369, key_3=
     # cipherText_1 = 0x65aa832af84e0bbc
     # print (format(cipherText_1, '016X'), format(cipherText_2, "016X"), "ciphertexts in start in hex")
 
-    # key_3 = 0x1918 #most significant
-    # key_2 = 0x121110
-    # key_1 = 0x0f0e0d0c0b0a0908
-    # key_0 = 0x0706050403020100 #least significant
+    # key_3 = 0x9ce73805171222e1  # most significant
+    # key_2 = 0x67fd8bae41f85b4a
+    # key_1 = 0xbde2432699ac471d
+    # key_0 = 0x4a99b7f867a1886c  # least significant
     print(format(key_3, '0x'), format(key_2, "0X"), format(
         key_1, "0X"), format(key_0, "0X"), "keys in hex")
 
@@ -61,44 +61,18 @@ def SIMON(plainText_1=0x74206e69206d6f6f, plainText_2=0x6d69732061207369, key_3=
     # sabit değer
     c = 2**(n) - 1
 
-    T = 0
-    j = 0
-    if (n == 16 and m == 4):
-        T = 32
-        j = 0
-    elif (n == 24 and m == 3):
-        T = 36
-        j = 0
-    elif (n == 24 and m == 4):
-        T = 36
-        j = 1
-    elif (n == 32 and m == 3):
-        T = 42
-        j = 2
-    elif (n == 32 and m == 4):
-        T = 44
-        j = 3
-    elif (n == 48 and m == 2):
-        T = 52
-        j = 2
-    elif (n == 48 and m == 3):
-        T = 54
-        j = 3
-    elif (n == 64 and m == 2):
-        T = 68
-        j = 2
-    elif (n == 24 and m == 3):
-        T = 69
-        j = 3
-    elif (n == 64 and m == 4):
-        T = 72
-        j = 4
-    else:
-        print("yanlış değer girdiniz\ndevam etmeyiniz")
+    T = 72
+    j = 4
 
     key_0_list = []
 
     def key_generation(key_0, key_1, key_2, key_3, m, T):
+        f = open("table_for_keys.txt", "w")
+        f.write(
+            "Round |      k_i+3     |      k_i+2     |      k_i+1     |      k_i+0   " + "\n")
+        f.write("start" + " |" + (format(key_3, '016X')) + "|" + format(key_2,
+                                                                        '016X') + "|" + format(key_1, '016X') + "|" + format(key_0, '016X') + "\n")
+
         key_0_n = key_0
         key_1_n = key_1
         key_2_n = key_2
@@ -139,6 +113,8 @@ def SIMON(plainText_1=0x74206e69206d6f6f, plainText_2=0x6d69732061207369, key_3=
             # print (key_0_n)
             # print ("key_0_n in hex",format(key_0_n, '016b'))
             for i in range(T):
+                round_n = i
+                # print(round_n)str(round_n)
                 # print ("key{} = {}".format(i, key_0_n))
                 # print ("key_0_n in hex",format(key_0_n, '016b'))
                 temp1 = (key_3 >> 3) | (key_3 << (n - 3)) & c
@@ -160,9 +136,13 @@ def SIMON(plainText_1=0x74206e69206d6f6f, plainText_2=0x6d69732061207369, key_3=
                 key_2 = key_3
                 key_3 = temp6
                 key_0_list.append(key_next)
+
+                f.write(str(round_n) + "     |" + (format(key_3, '016X')) + "|" + format(key_2,
+                        '016X') + "|" + format(key_1, '016X') + "|" + format(key_0_n, '016X') + "\n")
+
                 # print ("key{} = {}".format(i, key_next)
             return key_0_list
-
+        f.close()
     key_list = key_generation(key_0, key_1, key_2, key_3, m, T)
 
     # encryption
@@ -192,42 +172,6 @@ def SIMON(plainText_1=0x74206e69206d6f6f, plainText_2=0x6d69732061207369, key_3=
           format(text2_list[-1], "0X"), "ciphertext hex",)
     print("********************************************************************")
 
-    """
-    ### decryption
-    cp_text1_list= []
-    cp_text2_list= []
-    # def enc(plainText_1, plainText_2, key_0, T):
-    for k in reversed(key_0_list):
-        #plaintext = t1[0:3]t2[0:3]
-        ct1 = cipherText_1
-        ct2 = cipherText_2
-        # print ("before texts 01 in hex",format(t1, '04x'), format(t2, "04X"))
-        # print(key_0_list[i])
-        crol_1 = (ct1 << 1) + (ct1 >> (n-1)) & c
-        crol_2 = (ct1 << 2) + (ct1 >> (n-2)) & c
-        crol_8 = (ct1 << 8) + (ct1 >> (n-8)) & c
-        tmp1 = crol_1 & crol_8
-        tmp2 = ct2 ^ tmp1
-        tmp3 = tmp2 ^ crol_2
-        tmp4 = tmp3 ^ k
-        ct2 = ct1
-        ct1 = tmp4
-
-        #print(key_0)
-        # t1 = tmp
-        cp_text1_list.append(ct1)
-        cp_text2_list.append(ct2)
-        # print(t1, t2)
-
-        # print(text1_list)
-        # print(text2_list)
-        # print("key {} = {}".format(key_0_list.index(),k))
-        cipherText_1 = ct1
-        cipherText_2 = ct2
-    # print(t1,t2)
-        # return text1_list, text2_list
-    print (format(cp_text2_list[-1], '016X'), format(cp_text1_list[-1], "016X"), "plaintext hex")
-    """
     cipherText_1 = plainText_1
     cipherText_2 = plainText_2
 
@@ -241,21 +185,39 @@ cipherText_1 = 0
 cipherText_2 = 0
 IV_1 = 0x477f00c83731ec7b
 IV_2 = 0x96b9fb0ffeb25ade
+key_3 = 0x9ce73805171222e1  # most significant
+key_2 = 0x67fd8bae41f85b4a
+key_1 = 0xbde2432699ac471d
+key_0 = 0x4a99b7f867a1886c  # least significant
+
+round_n = 0
 # pt_1 = IV_1 ^ 0
 # pt_2 = IV_2 ^ 0
 # print(pt_1)
 # print(pt_2)
 f = open("random_numbers_simon_10k.txt", "w")
-a, b = SIMON(plainText_1=IV_1, plainText_2=IV_2)
+a, b = SIMON(plainText_1=IV_1, plainText_2=IV_2, key_3=key_3,
+             key_2=key_2, key_1=key_1, key_0=key_0)
 x = format(a, '064b')
 y = format(b, '064b')
 f.write(str(x) + str(y) + "\n")
-for i in range(1):
-    a, b = SIMON(plainText_1=a, plainText_2=b)
-    z = format(a, '064b')
-    t = format(b, '064b')
-    f.write(str(z) + str(t) + "\n")
+# for i in range(1):
+#     a, b = SIMON(plainText_1=a, plainText_2=b, key_3=key_3,
+#                  key_2=key_2, key_1=key_1, key_0=key_0)
+#     z = format(a, '064b')
+#     t = format(b, '064b')
+#     f.write(str(z) + str(t) + "\n")
 f.close()
+
+f = open("table_for_detail.txt", "w")
+f.write("SIMON Cipher 128-bit block, 256-bit key\n")
+f.write("plaintext:" + (format(IV_1, '0X')) + " " + format(IV_2, '0X') + "\n")
+f.write("key      :" + (format(key_3, '0X')) + " " + format(key_2,
+        '0X') + " " + format(key_1, '0X') + " " + format(key_0, '0X') + "\n")
+f.write("Round |  k_i+3  |  k_i+2  |  k_" + "\n")
+# f.write(str(round_n) + "safad")
+f.close()
+
 # # while 1 == 1:
 # #     a,b = SIMON(plainText_1 = a, plainText_2 = b)
 
